711c31de4d4e2c51c8ba7600a6e405188b9666c9
Saif Husain Ansari <saifhussainansari6@gmail.com>
2025-05-15 23:24:16 +0530

    system prompt added

--- apps/worker/index.ts
+++ apps/worker/index.ts
@@ -4,7 +4,8 @@ import Anthropic from "@anthropic-ai/sdk";
 import { prismaClient } from "db/client";
 import { RelayWebsocket } from "./ws";
 import { ArtifactProcessor } from "./parser";
-import { onFileUpdate, onShellCommand } from "./os";
+import { onFileUpdate, onPromptEnd, onShellCommand } from "./os";
+import { systemPrompt } from "./systemPrompt";
 
 const app = express();
 app.use(cors());
@@ -52,6 +53,45 @@ app.post("/prompt", async (req, res) => {
     orderBy:{createdAt: "asc"},
   });
   let artifactProcessor = new ArtifactProcessor("", (filePath, fileContent) => onFileUpdate(filePath, fileContent, projectId, promptDb.id, project.type), (shellCommand) => onShellCommand(shellCommand, projectId, promptDb.id));
-  
+  let artifact = "";
+
+  let response = client.messages.stream({
+    messages: allPrompts.map((p: any) => ({
+      role: p.type === "USER" ? "user" : "assistant",
+      content: p.content,
+    })),
+    system: systemPrompt(project.type),
+    model: "claude-3-7-sonnet-20250219",
+    max_tokens: 8000,
+  }).on('text', (text) => {
+    artifactProcessor.append(text);
+    artifactProcessor.parse();
+    artifact += text;
+  })
+  .on('finalMessage', async (message) => {
+    console.log("done!");
+    await prismaClient.prompt.create({
+      data: {
+        content: artifact,
+        projectId,
+        type: "SYSTEM",
+      },
+    });
+
+    await prismaClient.action.create({
+      data: {
+        content: "Done!",
+        projectId,
+        promptId: promptDb.id,
+      },
+    });
+    onPromptEnd(promptDb.id);
+  })
+  .on('error', (error) => {
+    console.log("error", error);
+  });
+
+  res.json({ response });
+
 
 });
